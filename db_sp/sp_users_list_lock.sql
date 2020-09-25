fa5249f18f4011e4637f27efb6ca13bf97dcc086
pancitpalabok <36721276+pancitpalabok@users.noreply.github.com>
2020-09-26 05:50:54 +0800

    DB Remove

--- db_sp/sp_users_list_lock.sql
+++
@@ -1,9 +0,0 @@
-DROP PROCEDURE IF EXISTS sp_users_list_lock;
-DELIMITER $$
-CREATE PROCEDURE sp_users_list_lock(
-	IN in_user_id INT
-) BEGIN
-	UPDATE atb_user SET user_locked = 1 WHERE user_id = in_user_id;
-	SELECT 'User Locked' h, 'You have successfully locked the user' m, 'success' s;
-END $$
-DELIMITER $$
